0e1aefc75752cad4423474a5b520f1777d25ae0c
Chris Croal <ccroal@ntlworld.com>
2019-05-28 20:07:06 +0100

    promise.all now returns array of stories

---
+++ src/components/NewsListComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NewsListComponent = (props) => {
+
+
+    return(
+      <div>
+        <h2>This is the NewsList</h2>
+        <h3>{console.log(props)}</h3>
+      </div>
+    )
+
+}
+
+export default NewsListComponent;
--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import NewsList from '../components/NewsList';
+import NewsListComponent from '../components/NewsListComponent';
 
 class NewsContainer extends Component {
 
@@ -11,13 +11,12 @@ class NewsContainer extends Component {
     }
     this.loadStories = this.loadStories.bind(this);
     this.makestoriesArray = this.makestoriesArray.bind(this);
-    this.getStory = this.getStory.bind(this);
+    this.getStoryInfo = this.getStoryInfo.bind(this);
 
   }
 
   componentDidMount(){
     this.loadStories('https://hacker-news.firebaseio.com/v0/topstories.json')
-
   }
 
 
@@ -25,37 +24,32 @@ class NewsContainer extends Component {
     fetch(url)
     .then((res) => {return res.json()})
     .then((data) => { return this.makestoriesArray(data)})
-    .then(storyId => this.getStory(storyId));
+     .then((storyIdArray) => this.getStoryInfo(storyIdArray))
 
 }
 
 
-  makestoriesArray(){
-    const topStoriesID = this.state.storyList.splice(19,20);
+  makestoriesArray(data){
+    const topStoriesID = data.splice(0,20);
+    this.setState({storyList: topStoriesID})
     return topStoriesID;
   }
 
-
-  getStory(stories){
-    const requests = stories.map(storyId => fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`));
+  getStoryInfo(stories){
+    const requests = stories.map(storyId => fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`))
     Promise.all(requests)
-      .then(responses => {
-        for(let response of responses) {
-        alert(`${response.url}: ${response.status}`);
-      }
-      return responses;
-    })
-      .then(responses => Promise.all(responses.map(response => response.json())))
-      .then(data => this.setState({stories: data}))
+    .then(responses => Promise.all(responses.map(response => response.json())))
+            .then(data => this.setState({stories: data}))
+    }
 
-}
+
 
   render(){
 
     return(
       <div>
         <h2>Latest Stories</h2>
-        <NewsList stories={this.state.storyList}/>
+        <NewsListComponent stories={this.state.stories}/>
       </div>
     );
   }
